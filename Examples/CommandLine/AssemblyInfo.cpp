8991a15a9e99ef4c0bf5ed070ffd74ec0ef0affc
Alan Pereira <alan.pereira@meitech.com.br>
2025-10-06 11:14:40 -0300

    Refactor CommandLine sample to C++

---
+++ Examples/CommandLine/AssemblyInfo.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+
+[assembly:AssemblyTitleAttribute("CommandLine")];
+[assembly:AssemblyDescriptionAttribute("HBM Automation API command line sample rewritten in C++/CLI")];
+[assembly:AssemblyCompanyAttribute("Hottinger Baldwin Messtechnik GmbH")];
+[assembly:AssemblyProductAttribute("Automation API Samples")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c) Hottinger Baldwin Messtechnik GmbH")];
+[assembly:ComVisible(false)];
+[assembly:AssemblyVersionAttribute("1.0.0.0")];
+[assembly:AssemblyFileVersionAttribute("1.0.0.0")];
